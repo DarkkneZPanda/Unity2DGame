65e51f4c67fd36ff937ba9b6f8f86230271c5339
DarkkneZPanda <ortegaluis1114@gmail.com>
2023-01-31 22:08:41 -0800

    Added them back 2

---
+++ Maze-Floors/Assets/SettingsOptions.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsOptions : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public void SetVolume (float volume) {
+        audioMixer.SetFloat("Volume", Mathf.Log10(volume) * 20);
+    }
+}
