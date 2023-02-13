b3be1e9ba6247a4d3d9aaaffef2fa69b6d7930cc
DarkkneZPanda <ortegaluis1114@gmail.com>
2023-02-13 12:59:55 -0800

    Better Collectible System

---
+++ Maze-Floors/Assets/Coin.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Coin : MonoBehaviour, ICollectible
+{
+    [SerializeField] private AudioSource Coinpickup;
+    private void OnEnable() {
+        Coin.OnCoinCollected += CoinSound;
+    }
+
+    private void OnDisable() {
+        Coin.OnCoinCollected -= CoinSound;
+    }
+    public static event Action OnCoinCollected;
+    public void Collect()
+    {
+        Destroy(gameObject);
+        OnCoinCollected?.Invoke();
+    }
+
+    public void CoinSound() {
+        Coinpickup.Play();
+    }
+}
---
+++ Maze-Floors/Assets/CoinCount.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CoinCount : MonoBehaviour
+{
+    public TextMeshProUGUI coinCount;
+    int count;
+
+    private void OnEnable() {
+        Coin.OnCoinCollected += IncrementCoinCount;
+    }
+
+    private void OnDisable() {
+        Coin.OnCoinCollected -= IncrementCoinCount;
+    }
+
+    public void IncrementCoinCount() {
+        count++;
+        coinCount.text = $"Coins: {count}";
+    }
+}
